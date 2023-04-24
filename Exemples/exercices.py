aba6858b8184cfbc4da12361000b142d56d8e119
At0micTurtle <carltrep@videotron.ca>
2023-04-23 21:09:14 -0400

    Update du fichier exercices

--- Exemples/exercices.py
+++ Exemples/exercices.py
@@ -61,3 +61,121 @@ print("Supprimer les doublons")
 noms = ["Jean", "Marc", "Pierre", "Paul", "Jacques", "Marie", "Lucie", "Jean", "Marc", "Pierre", "Paul", "Jacques", "Marie", "Lucie"]
 set_noms = list(set(noms)) # Convertir un set en liste
 print(set_noms)
+
+print()
+
+# Mots communs entre deux phrases
+print("Mots communs entre deux phrases")
+
+string1 = "Bonjour tout le monde"
+string2 = "Bonjour tout le monde et bienvenue"
+
+def get_common_words(string1, string2):
+    words1 = string1.split(" ")
+    words2 = string2.split(" ")
+    common_words = []
+    for word in words1:
+        if word in words2:
+            common_words.append(word)
+    return common_words
+
+print(get_common_words(string1, string2))
+
+print()
+
+# Trouver les nombres pairs et impairs dans une liste
+print("Trouver les nombres pairs et impairs dans une liste")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def get_even_and_odd_numbers(numbers):
+    even_numbers = []
+    odd_numbers = []
+    for number in numbers:
+        if number % 2 == 0:
+            even_numbers.append(number)
+        else:
+            odd_numbers.append(number)
+    return even_numbers, odd_numbers
+
+even_numbers, odd_numbers = get_even_and_odd_numbers(numbers)
+print("Nombres pairs:", even_numbers)
+print("Nombres impairs:", odd_numbers)
+
+print()
+
+# Trouver les nombres premiers dans une liste
+print("Trouver les nombres premiers dans une liste")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False 
+    return True
+
+def get_prime_numbers(numbers):
+    prime_numbers = []
+    for number in numbers:
+        if is_prime(number):
+            prime_numbers.append(number)
+    return prime_numbers
+
+print(get_prime_numbers(numbers))
+
+print()
+
+# Trouver les nombres premiers entre deux nombres
+print("Trouver les nombres premiers entre 1 et 100")
+def get_prime_numbers_between(min, max):
+    prime_numbers = []
+    for number in range(min, max + 1):
+        if is_prime(number):
+            prime_numbers.append(number)
+    return prime_numbers
+
+print(get_prime_numbers_between(1, 100))
+
+print()
+
+# Données monotones
+print("Données monotones")
+
+def is_monotonic(numbers):
+    if len(numbers) < 2:
+        return True
+    for i in range(1, len(numbers)):
+        if numbers[i] < numbers[i - 1]:
+            return False
+    return True
+
+print(is_monotonic([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(is_monotonic([1, 2, 3, 4, 5, 6, 7, 8, 9, 8]))
+
+print()
+
+# Trouver les éléments manquants
+print("Trouver les éléments manquants")
+
+a = [2, 3, 5, 8]
+def get_missing_elements(numbers):
+    missing_elements = []
+    for i in range(1, len(numbers)):
+        if numbers[i] - numbers[i - 1] > 1:
+            for j in range(numbers[i - 1] + 1, numbers[i]):
+                missing_elements.append(j)
+    return missing_elements
+
+def get_missing_elements2(l, min, max):
+    missing_elements = []
+    for i in range(min, max + 1):
+        if i not in l:
+            missing_elements.append(i)
+    return missing_elements
+
+print(get_missing_elements([1, 4, 5, 8, 9]))
+print(get_missing_elements([1, 2, 3, 4, 5, 6, 7, 8, 9, 11]))
+print(get_missing_elements2(a, 1, 10))
+
+print()
