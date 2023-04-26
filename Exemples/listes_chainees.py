39bf92e6484ca3d0caa3d1bd1b52972a7765c5fe
At0micTurtle <carltrep@videotron.ca>
2023-04-26 00:28:15 -0400

    Modification du fichier sur les listes chainées

--- Exemples/listes_chainees.py
+++ Exemples/listes_chainees.py
@@ -32,6 +32,14 @@ class LinkedList:
             node = node.next
         return nb_nodes
     
+    def get_all_data_as_list(self):
+        node = self.head
+        data = []
+        while node is not None:
+            data.append(node.data)
+            node = node.next
+        return data
+    
 class Node:
     def __init__(self, data):
         self.data = data
@@ -51,14 +59,41 @@ node3.set_next(node4)
 
 linked_list = LinkedList(node1)
 print("Nombre de noeuds:", linked_list.get_nb_nodes())
+print("Toutes les données:", linked_list.get_all_data_as_list())
 
-# Affichage d'une liste chainée
-def print_list(head):
-    node = head
-    while node is not None:
-        print(node.data)
-        node = node.next
-
-print_list(node1)
+print()
 
 # Insertion d'un élément dans une liste chainée
+def insert_node(linked_list, node, index):
+    if index == 0:
+        node.next = linked_list.head
+        linked_list.head = node
+    else:
+        previous_node = linked_list.head
+        for i in range(index-1):
+            previous_node = previous_node.next
+        node.next = previous_node.next
+        previous_node.next = node
+
+node5 = Node("Hello")
+insert_node(linked_list, node5, 2)
+print("Nombre de noeuds:", linked_list.get_nb_nodes())
+print("Toutes les données:", linked_list.get_all_data_as_list())
+
+print()
+
+# Suppression d'un élément dans une liste chainée
+def remove_node(linked_list, index):
+    if index == 0:
+        linked_list.head = linked_list.head.next
+    else:
+        previous_node = linked_list.head
+        for i in range(index-1):
+            previous_node = previous_node.next
+        previous_node.next = previous_node.next.next
+
+remove_node(linked_list, 2)
+print("Nombre de noeuds:", linked_list.get_nb_nodes())
+print("Toutes les données:", linked_list.get_all_data_as_list())
+
+print()
